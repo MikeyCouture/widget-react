2ba3bd498c6ac4135c5bad8fa3264ad808b0798d
MikeyCouture <michelcouture42@gmail.com>
2021-03-30 19:56:42 -0400

    first commit of widgets exercises

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordion from "./Components/Accordion";
+import Search from "./Components/Search";
+import Dropdown from "./Components/Dropdown";
 
-function App() {
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework"
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favourite JS library among engineers"
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components"
+  }
+];
+
+const options = [
+  {
+    label: "The Colour Red",
+    value: "red"
+  },
+  {
+    label: "The Colour Green",
+    value: "green"
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue"
+  }
+];
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Accordion items={items} /> */}
+      {/* <Search /> */}
+      <Dropdown options={options} />
     </div>
   );
-}
-
-export default App;
+};
